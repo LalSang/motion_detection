0b5c76921f9f25f57591346dff8263bc0b8a87ed
LalSang <thomassang07@gmail.com>
2026-01-17 21:13:31 -0500

    Read Single Grey Frame and Add Blue

--- cam_test.py
+++ cam_test.py
@@ -9,15 +9,13 @@ vc = cv2.VideoCapture(0)
 
 # Here we are checking if the webcam opens 
 # If not we throw an runtime error 
-# if vc.isOpened():
-#     rval = True
-# else:
-#     rval = False
-#     raise RuntimeError("Could not open webcam")
-
 # If webcam don't open then raise an runtimeerror 
 if not vc.isOpened():
     raise RuntimeError("Could not open webcam")
+
+rval, frame = vc.read()
+prev_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+prev_grey = cv2.GaussianBlur(prev_grey, (21,21), 0)
 
 # Here we are checking if webcam is reading frames 
 # if not then we break 
